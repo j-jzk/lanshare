f0d8de2e7ab6381df85c5033d20b08342e15f689
j-jzk <jjezek@protonmail.com>
2023-04-17 11:40:20 +0200

    Add a switch to disable uploads

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", HandleDownload)
-	http.HandleFunc("/__lanshare_upload", HandleUpload) // TODO: differentiate using HTTP methods instead of a special URL
+	allowUploads := false
+
+	http.Handle("/", &DownloadHandler{allowUploads: allowUploads})
+	if allowUploads {
+		http.HandleFunc("/__lanshare_upload", HandleUpload) // TODO: differentiate using HTTP methods instead of a special URL
+	}
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
